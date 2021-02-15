3a3f23becb8215739702242b7ad2ab69a1d9911a
fn3x <fn3x@yandex.ru>
2021-02-16 01:09:56 +0300

    Created master branch

---
+++ src/main/java/GetHealthBot.java
@@ -0,0 +1,31 @@
+import org.telegram.telegrambots.bots.*;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class GetHealthBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(String.valueOf(update.getMessage().getChatId()));
+            message.setText("Привет, " + update.getMessage().getFrom().getFirstName() + "!");
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "gethealth_bot";
+    }
+
+    public String getBotToken() {
+        return "1490694563:AAHc7ke2bDhKaT4jwsOZRIM-BZJj8fIqeJM";
+    }
+}
